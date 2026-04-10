12b2e97fa2b2dc3a8a4ac5b5878aa5e284a7ab91
wicktahir-coder <wicktahir@gmail.com>
2026-04-10 15:33:03 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,16 +10,11 @@ import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/messageroute.js";
 import { io, server, app } from "./lib/socket.io.js";
 
-
-
-
-app
-
 const PORT = process.env.PORT;
 
 const __dirname = path.resolve();
 
-app.use(express.json({limit: "10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
@@ -29,7 +24,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
